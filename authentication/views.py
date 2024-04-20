fc316b34a7cb08048a3f0e04217c3dba212c590b
hilaldfzn <hilalfauzan9@gmail.com>
2024-04-20 08:14:48 +0700

    [Feat] Create UI for login and register page

--- authentication/views.py
+++ authentication/views.py
@@ -6,5 +6,11 @@ def authentication(request):
 def register(request):
   return render(request, 'register.html')
 
+def register_pengguna(request):
+  return render(request, 'registration_form/register_pengguna.html')
+
+def register_label(request):
+  return render(request, 'registration_form/register_label.html')
+
 def login(request):
   return render(request, 'login.html')
