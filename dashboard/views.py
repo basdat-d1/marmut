1e8602d8a3fae3250d6dba1591ace52558d31475
hilaldfzn <hilalfauzan9@gmail.com>
2024-05-16 14:16:15 +0700

    [Feat] Implement backend for authentication and dashboard

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,224 +1,144 @@
+from utils.query import connectdb
+from django.urls import reverse
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.db.backends.utils import CursorWrapper
 
-def dashboard_pengguna_biasa(request):
-    playlists = [
-        {
-            'judul': 'Argue get',
-            'jumlah_lagu': 11,
-            'total_durasi': 4596
-        },
-        {
-            'judul': 'Health say easy',
-            'jumlah_lagu': 91,
-            'total_durasi': 4188
-        },
-    ]
+@connectdb
+def dashboard_pengguna(cursor: CursorWrapper, request):
+    email = request.session.get('email')
 
-    dummy_pengguna_biasa = {
-        "nama": "Muhammad Hilal Darul Fauzan",
-        "email": "hilalfauzan9@gmail.com",
-        "kota_asal": "Jakarta",
-        "gender": "Laki-laki",
-        "tempat_lahir": "California",
-        "tanggal_lahir": "27 April 2004",
-        "role": "Pengguna Biasa",
-        "playlists": playlists
-    }
+    cursor.execute("SELECT * FROM AKUN WHERE email = %s", [email])
+    user = cursor.fetchone()
 
-    return render(request, 'pengguna/dashboard_pengguna_biasa.html', dummy_pengguna_biasa)
+    if not user:
+        return HttpResponseRedirect(reverse('authentication:login'))
 
-def dashboard_premium(request):
-    playlists = [
-        {
-            'judul': 'Radio ground everyone',
-            'jumlah_lagu': 39,
-            'total_durasi': 6664
-        },
-        {
-            'judul': 'Once respond voice',
-            'jumlah_lagu': 49,
-            'total_durasi': 6682
-        },
-    ]
+    is_artist = request.session.get('is_artist', False)
+    is_songwriter = request.session.get('is_songwriter', False)
+    is_podcaster = request.session.get('is_podcaster', False)
+    status_langganan = request.session.get('status_langganan', 'Non-Premium')
 
-    dummy_premium = {
-        "nama": "Robert Vang",
-        "email": "donnaherring@gmail.com",
-        "kota_asal": "Lake Williamfurt",
-        "gender": "Laki-laki",
-        "tempat_lahir": "New Bridgettown",
-        "tanggal_lahir": "23 April 1998",
-        "role": "Pengguna Biasa",            # Dummy premium ini sebagai pengguna biasa yang berlangganan
-        "playlists": playlists
-    }
+    records_song_artist, records_song_songwriter, records_podcast = [], [], []
 
-    return render(request, 'pengguna/dashboard_premium.html', dummy_premium)
+    id_artist, id_songwriter, id_pemilik_hak_cipta_artist, id_pemilik_hak_cipta_songwriter = fetch_user_data(cursor, email, records_song_artist, records_song_songwriter, records_podcast)
 
-def dashboard_podcaster(request):
-    podcasts = [
-        {
-            'judul': 'Record girl',
-            'jumlah_episode': 5,
-            'total_durasi': 167
-        },
-        {
-            'judul': 'Their thought discover',
-            'jumlah_episode': 10,
-            'total_durasi': 170
-        },
-    ]
+    records_user_playlist = fetch_user_playlist(cursor, email)
 
-    dummy_podcaster = {
-        "nama": "Beth White",
-        "email": "hilljason@gmail.com",
-        "kota_asal": "Port Bradleyburgh",
-        "gender": "Laki-laki",
-        "tempat_lahir": "Ryanmouth",
-        "tanggal_lahir": "14 July 2000",
-        "role": "Podcaster",
-        "podcasts": podcasts
-    }
+    role_verified_list = []
+    if is_artist:
+        role_verified_list.append('Artist')
+    if is_songwriter:
+        role_verified_list.append('Songwriter')
+    if is_podcaster:
+        role_verified_list.append('Podcaster')
+    
+    role_verified = ', '.join(role_verified_list) if role_verified_list else "Pengguna Biasa"
 
-    return render(request, 'pengguna/dashboard_podcaster.html', dummy_podcaster)
-
-def dashboard_artist_songwriter(request):
-    songs = [
-        {
-            'judul': 'Travel against my city',
-            'total_play': 142153,
-            'total_download': 10232
-        },
-        {
-            'judul': 'Their thought discover',
-            'total_play': 213243,
-            'total_download': 9921
-        },
-    ]
-
-    dummy_artist_songwriter = {
-        "nama": "Kelly Walsh",
-        "email": "barbaragreen@yahoo.com",
-        "kota_asal": "Michaelstad",
-        "gender": "Perempuan",
-        "tempat_lahir": "Jonesbury",
-        "tanggal_lahir": "21 September 1988",
-        "role": "Artist/Songwriter",
-        "songs": songs
-    }
-
-    return render(request, 'pengguna/dashboard_artist_songwriter.html', dummy_artist_songwriter)
-
-def dashboard_label(request):
-    albums = [
-        {
-            'judul': 'Enterprise-wide incremental superstructure',
-            'jumlah_lagu': 85,
-            'total_durasi': 421
-        },
-        {
-            'judul': 'Reactive uniform product',
-            'jumlah_lagu': 20,
-            'total_durasi': 326
-        },
-    ]
-
-    dummy_label = {
-        "nama": "Singleton, Welch and Rios",
-        "email": "derek22@hotmail.com",
-        "kontak": "7185100571",
-        "albums": albums
-    }
-
-    return render(request, 'label/dashboard_label.html', dummy_label)
-
-def dashboard(request):
-    if "email" not in request.session:
-        return redirect('authentication:login')
-
-    email = request.session["email"]
-    role = request.session["role"]
-
-    with conn.cursor() as cursor:
-        cursor.execute("set search_path to marmut")
-        cursor.execute(f"SELECT * FROM AKUN WHERE email = '{email}'")
-        user_data = cursor.fetchone()
-
-        if not user_data:
-            return redirect('authentication:login')
-        
-        cursor.execute(f"SELECT * FROM PREMIUM WHERE email = '{email}'")
-        premium = cursor.fetchone()
-        if premium:
-            is_premium = True
-        else:
-            is_premium = False
-        
-        cursor.execute("set search_path to public")
-
-    roles = get_role_pengguna(email)
     context = {
-        'is_logged_in': True,
-        'user': user_data,
-        'role': role,
-        'roles': roles,
-        'is_premium': is_premium
+        'role': 'pengguna',
+        'status': 'success',
+        'role_verified': role_verified,
+        'nama': user[2],
+        'email': user[0],
+        'status_langganan': status_langganan,
+        'kota_asal': user[7],
+        'gender': user[3],
+        'tempat_lahir': user[4],
+        'tanggal_lahir': user[5],
+        'isArtist': is_artist,
+        'isSongwriter': is_songwriter,
+        'isPodcaster': is_podcaster,
+        'records_user_playlist': records_user_playlist,
+        'records_song_artist': records_song_artist,
+        'records_song_songwriter': records_song_songwriter,
+        'records_podcast': records_podcast,
     }
-    if ("Artist" in roles and "Songwriter" in roles):
-        context['songs'] = get_songs_artist_songwriter(email)
 
-    return render(request, 'dashboard.html', context)
+    return render(request, 'dashboard_pengguna.html', context)
 
+@connectdb
+def dashboard_label(cursor: CursorWrapper, request):
+    email = request.session.get('email')
 
-def get_role_pengguna(email: str) -> list:
-    roles = []
-    with conn.cursor() as cursor:
-        cursor.execute("set search_path to marmut")
-        cursor.execute(f"SELECT * FROM ARTIST WHERE email_akun = '{email}'")
-        artist = cursor.fetchall()
-        cursor.execute(f"SELECT * FROM SONGWRITER WHERE email_akun = '{email}'")
-        songwriter = cursor.fetchall()
-        cursor.execute(f"SELECT * FROM PODCASTER WHERE email = '{email}'")
-        podcaster = cursor.fetchall()
-        cursor.execute("set search_path to public")
-    if len(artist) > 0:
-        roles.append("Artist")
-    if len(songwriter) > 0:
-        roles.append("Songwriter")
-    if len(podcaster) > 0:
-        roles.append("Podcaster")
+    cursor.execute("SELECT * FROM LABEL WHERE email = %s", [email])
+    label = cursor.fetchone()
 
-    return roles
+    if not label:
+        return HttpResponseRedirect(reverse('authentication:login'))
 
-def get_songs_artist_songwriter(email: str) -> list:
-    songs = []
-    formatted_songs = []
-    with conn.cursor() as cursor:
-        cursor.execute("set search_path to marmut")
-        cursor.execute(f"SELECT id FROM ARTIST WHERE email_akun = '{email}'")
-        id_json = cursor.fetchall()
-        id_searched = str(id_json[0][0])
+    id_label = label[0]
+    cursor.execute("SELECT * FROM ALBUM WHERE id_label = %s", [id_label])
+    records_album = cursor.fetchall()
 
-        cursor.execute(f"SELECT * FROM SONG WHERE id_artist = '{id_searched}'")
-        datas = cursor.fetchall()
+    context = {
+        'role': 'label',
+        'status': 'success',
+        'id': label[0],
+        'nama': label[1],
+        'email': label[2],
+        'kontak': label[4],
+        'id_pemilik_hak_cipta': label[5],
+        'records_album': records_album,
+    }
 
-        for data in datas:
-            id_konten = str(data[0])
-            cursor.execute(f"SELECT * FROM KONTEN WHERE id = '{id_konten}'")
-            tmp = cursor.fetchall()
-            songs.append(tmp)
+    return render(request, 'dashboard_label.html', context)
 
-        for song_group in songs:
-            group_list = []
-            for song in song_group:
-                song_dict = {
-                    'id': song[0],
-                    'title': song[1],
-                    'release_date': song[2],
-                    'year': song[3],
-                    'duration': song[4]
-                }
-                group_list.append(song_dict)
-            formatted_songs.append(group_list)
-        cursor.execute("set search_path to public")
+def fetch_user_data(cursor: CursorWrapper, email, records_song_artist, records_song_songwriter, records_podcast):
+    id_artist, id_songwriter, id_pemilik_hak_cipta_artist, id_pemilik_hak_cipta_songwriter = "", "", "", ""
 
-    return formatted_songs
+    cursor.execute("SELECT * FROM ARTIST WHERE email_akun = %s", [email])
+    artist = cursor.fetchone()
+    if artist:
+        id_artist = artist[0]
+        id_pemilik_hak_cipta_artist = artist[2]
+        cursor.execute("SELECT * FROM SONG WHERE id_artist = %s", [id_artist])
+        artist_songs = cursor.fetchall()
+        for song in artist_songs:
+            id_song = song[0]
+            cursor.execute("SELECT judul, durasi FROM KONTEN WHERE id = %s", [id_song])
+            additional_detail_song = cursor.fetchone()
+            if additional_detail_song:
+                records_song_artist.append(song + additional_detail_song)
+
+    cursor.execute("SELECT * FROM SONGWRITER WHERE email_akun = %s", [email])
+    songwriter = cursor.fetchone()
+    if songwriter:
+        id_songwriter = songwriter[0]
+        id_pemilik_hak_cipta_songwriter = songwriter[2]
+        cursor.execute("SELECT id_song FROM SONGWRITER_WRITE_SONG WHERE id_songwriter = %s", [id_songwriter])
+        list_id_song = cursor.fetchall()
+        for song in list_id_song:
+            id_song = song[0]
+            cursor.execute("SELECT * FROM SONG WHERE id_konten = %s", [id_song])
+            records_song_awal = cursor.fetchone()
+            cursor.execute("SELECT judul, durasi FROM KONTEN WHERE id = %s", [id_song])
+            additional_detail_song = cursor.fetchone()
+            if records_song_awal and additional_detail_song:
+                records_song_songwriter.append(records_song_awal + additional_detail_song)
+
+    cursor.execute("SELECT * FROM PODCASTER WHERE email = %s", [email])
+    podcaster = cursor.fetchone()
+    if podcaster:
+        cursor.execute("SELECT * FROM PODCAST WHERE email_podcaster = %s", [email])
+        list_id_podcast = cursor.fetchall()
+        for podcast in list_id_podcast:
+            id_podcast = podcast[0]
+            cursor.execute("""
+                SELECT k.id, k.judul, COUNT(*) AS jumlah_episode, k.durasi 
+                FROM KONTEN AS k 
+                JOIN EPISODE AS e ON e.id_konten_podcast = k.id 
+                WHERE k.id = %s 
+                GROUP BY k.id
+            """, [id_podcast])
+            records_podcast.append(cursor.fetchone())
+
+    return id_artist, id_songwriter, id_pemilik_hak_cipta_artist, id_pemilik_hak_cipta_songwriter
+
+def fetch_user_playlist(cursor: CursorWrapper, email):
+    cursor.execute("SELECT * FROM USER_PLAYLIST WHERE email_pembuat = %s", [email])
+    return cursor.fetchall()
+
+def is_premium(cursor: CursorWrapper, email):
+    cursor.execute("SELECT * FROM PREMIUM WHERE email = %s", [email])
+    return bool(cursor.fetchone())
--- utils/query.py
+++ utils/query.py
@@ -1,36 +1,11 @@
+from functools import wraps
 from django.db import connection
 
 def connectdb(func):
-    def wrapper(request):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
         with connection.cursor() as cursor:
-            cursor.execute("SET search_path to MARMUT;")
-            return func(cursor, request)
+            # cursor.execute("SET search_path to MARMUT;")
+            return func(cursor, *args, **kwargs)
         
     return wrapper
-
-# def get_db_connection():
-#     return psycopg2.connect(
-#         dbname=settings.DATABASES['default']['NAME'],
-#         user=settings.DATABASES['default']['USER'],
-#         password=settings.DATABASES['default']['PASSWORD'],
-#         host=settings.DATABASES['default']['HOST'],
-#         port=settings.DATABASES['default']['PORT']
-#     )
-
-# def run_query(query, params=None):
-#     connection = get_db_connection()
-#     cursor = connection.cursor()
-
-#     try:
-#         cursor.execute(query, params)
-
-#         if query.upper().startswith('SELECT'):
-#             return cursor.fetchall()
-#         else:
-#             connection.commit()
-#     except Exception as e:
-#         connection.rollback()
-#         return str(e)
-#     finally:
-#         cursor.close()
-#         connection.close()
