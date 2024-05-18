7fdccf23d26e1728691d06bc416df312fa5f82a1
joydebora <joy.debora@ui.ac.id>
2024-05-19 03:56:56 +0700

    fixing backend routing to play_user

--- downloaded_songs/urls.py
+++ downloaded_songs/urls.py
@@ -1,10 +1,10 @@
-# urls.py
 from django.urls import path
-from .views import downloaded_songs, get_downloaded_songs
+from .views import downloaded_songs, get_downloaded_songs, delete_song
 
 app_name = 'downloaded_songs'
 
 urlpatterns = [
     path('', downloaded_songs, name='downloaded_songs'),
     path('api/get_downloaded_songs/', get_downloaded_songs, name='get_downloaded_songs'),
+    path('delete-song/<uuid:song_id>/', delete_song, name='delete_song_view'),
 ]
